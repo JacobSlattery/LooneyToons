59f3dc08c7aabb35d5504bf4ff79128f8b69cc95
David Spurlock <DavidMichaelSpurlock@live.com>
2018-04-15 23:08:37 -0400

    changed to fragmentactivity and implemented locationlistener

--- app/src/main/java/com/example/timbe/looneytoons/MainActivity.java
+++ app/src/main/java/com/example/timbe/looneytoons/MainActivity.java
@@ -1,21 +1,55 @@
 package com.example.timbe.looneytoons;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+
+public class MainActivity extends FragmentActivity implements LocationListener {
+
+
+    private static final String GOOGLE_API_KEY = "AIzaSSDFSDF8Kv2eP0PM8adf5dSDFysdfas323SD3HA";
+    GoogleMap googleMap;
+    EditText placeText;
+    double latitude = 0;
+    double longitude = 0;
+    private int PROXIMITY_RADIUS = 5000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
