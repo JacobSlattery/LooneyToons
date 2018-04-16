5e8a9713772af107d1704ac8145fbece0dc02001
jloon <jlooney2@my.westga.edu>
2018-04-15 22:58:10 -0400

    Added map layout

--- app/src/main/java/com/example/timbe/looneytoons/MainActivity.java
+++ app/src/main/java/com/example/timbe/looneytoons/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.timbe.looneytoons;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
 
 }
